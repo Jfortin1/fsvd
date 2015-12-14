cc41b23d6051e9b45a6f848e068528963d96ba4c
jfortin <jfortin@jhsph.edu>
2015-12-14 12:00:09 -0500

    First push

---
+++ R/getBootSVDs.R
@@ -0,0 +1,24 @@
+# This function accepts truncated svds as well. 
+getBootSVDs <- function(u,v,d, B=100){
+	n <- nrow(v)
+	svds <- pblapply(1:B, function(b){
+		# Boot indices:
+		Pb <- matrix(0, n,n)
+		Pb[cbind(sample(n, replace=TRUE), 1:n)] <- 1
+
+		# Calculating bootstrapped svd:
+		DVPb <- diag(d) %*% t(v) %*% Pb
+		Psvd <- svd(DVPb)
+		ub <- u %*% ab
+		vb <- Psvd$v
+
+		# Taking care of the arbitrary signs:
+		ab <- Psvd$u
+		signs <- sign(diag(ab))
+		ub <- t(t(ub) * signs)
+		vb <- vb * signs
+
+		list(u=ub, v=vb, d=db)
+	})
+	svds
+}
